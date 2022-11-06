0b699645370c3b5ea9d56c41d360f5374c4393f0
icallhacks <rowandoorneweerd9@gmail.com>
2022-11-06 20:51:35 +0100

    Reload Command Fix

--- src/main/java/me/rowanscripts/doublelife/commands/reload.java
+++ src/main/java/me/rowanscripts/doublelife/commands/reload.java
@@ -8,6 +8,10 @@ import org.bukkit.command.CommandSender;
 public class reload {
 
     public static boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!ConfigHandler.configFile.exists()) {
+            sender.sendMessage(ChatColor.DARK_RED + "Couldn't find the configuration file! Have you deleted it? If so, please either restart or reload the server.");
+            return true;
+        }
         ConfigHandler.load();
         sender.sendMessage(ChatColor.DARK_GREEN + "You've reloaded the configuration file!");
         return true;
